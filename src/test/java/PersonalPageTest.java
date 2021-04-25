cba90dbb63da5297ed37f4ab8775ba58023f4a50
TimofeevaNastia <timofee.1997@mail.ru>
2021-04-25 23:34:39 +0400

    Commit 2

--- src/test/java/PersonalPageTest.java
+++ src/test/java/PersonalPageTest.java
@@ -36,7 +36,7 @@ public class PersonalPageTest extends BaseClass{
     @Step("Заполнения данных О себе авторизированного пользователя")
     @Test
     public void setPersonalData() throws IOException {
-        //d
+        //ddd
         AboutYourself aboutYourself=new AuthorizationPage(driver).auth(cfg.login(),cfg.passwors())
         .personalAccount().aboutYourself();
         logger.info("Начало");
